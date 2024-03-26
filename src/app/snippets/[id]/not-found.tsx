653a1f746fc6e1084e29a06982532523ce110d73
Treasure56 <131236913+Treasure56@users.noreply.github.com>
2024-03-26 15:53:11 +0100

    Added files for testing

--- src/app/snippets/[id]/not-found.tsx
+++ src/app/snippets/[id]/not-found.tsx
@@ -2,7 +2,7 @@ export default function SnippetNotFound() {
   return (
     <div>
       <h1 className="text-xl bold">
-        Sorry but we coulden't find that particular snipet
+        Sorry but we coulden&apos;t find that particular snipet
       </h1>
     </div>
   );
